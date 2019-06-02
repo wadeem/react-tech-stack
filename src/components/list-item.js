3ccb53fe5c00cd0231029ec4085ad55f2974af80
max3d <vadim@unikivi.ee>
2019-06-03 00:18:06 +0300

    selection reducer updates

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,15 +1,22 @@
 import React, {Component} from "react";
-import {Text} from "react-native";
-import {CardSection} from "./common"
+import {Text, TouchableWithoutFeedback, View} from "react-native";
+import {CardSection} from "./common";
+import * as actions from "../actions";
+import {connect} from "react-redux";
 
 class ListItem extends Component {
 
     render() {
         const {library} = this.props;
+        const {id, title} = library;
 
-        return <CardSection>
-            <Text style={styles.listStyle}>{library.title}</Text>
-        </CardSection>
+        return <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+            <View>
+                <CardSection>
+                    <Text style={styles.listStyle}>{title}</Text>
+                </CardSection>
+            </View>
+        </TouchableWithoutFeedback>
     }
 }
 
@@ -20,5 +27,8 @@ const styles = {
     }
 };
 
+const mapStateToProps = (state) => {
+    return {selectedLibraryId: state.selectedLibraryId}
+};
 
-export default ListItem;
+export default connect(mapStateToProps, actions)(ListItem);
--- src/reducers/selection-reducer.js
+++ src/reducers/selection-reducer.js
@@ -1,3 +1,5 @@
-export default () => {
-    return null;
+export default (state = null, action) => {
+    console.log(action);
+    if (action === "select_library") return action.payload;
+    return state;
 };
